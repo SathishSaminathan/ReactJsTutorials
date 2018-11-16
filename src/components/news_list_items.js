afce0c92ca6045b5b0e4bb082814fe60ea67a9aa
SathishSaminathan <Sathishcse1975@gmail.com>
2018-11-16 18:07:37 +0530

    commit_for_the_glamor_package

--- src/components/news_list_items.js
+++ src/components/news_list_items.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { css } from "glamor";
+
 // const NewsItem = ({items}) => {  /** In this we are using structuring like { Component } from "react", In this we are instructing to get the "{ items }" from the "props" array */
 //   console.log()
 //   return (
@@ -11,13 +13,33 @@ import React from "react";
 // };
 
 const NewsItem = props => {
-    console.log(props)
-    return (
-        <div onClick={() => alert("You have click " + props.items.id + " element")}>
-            <h3>{props.items.title}</h3>
-            <div>{props.items.feed}</div>
-        </div>
-    );
+  console.log(props);
+
+  let news_item = css({
+    padding: "20px",
+    boxSizing: "border-box",
+    ":hover": {
+      color: "red"
+    },
+    "@media(max-width: 500px)": {
+      color: "blue"
+    }
+  });
+
+  let item_grey = css({
+    background: "lightgrey"
+  });
+  return (
+    <div
+    //   {...news_item}                     // This will grab all the properties inside the "news_item" element and put them inside here 
+    //   {...item_grey}
+    className={`${news_item} ${item_grey}`} // This is called Template Strings
+      onClick={() => alert("You have click " + props.items.id + " element")}
+    >
+      <h3>{props.items.title}</h3>
+      <div>{props.items.feed}</div>
+    </div>
+  );
 };
 
 export default NewsItem;
