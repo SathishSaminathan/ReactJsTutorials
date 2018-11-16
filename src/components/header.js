1ce76c00e7c69031062aceb6affa3bc7bb78701a
SathishSaminathan <Sathishcse1975@gmail.com>
2018-11-16 12:21:08 +0530

    initial_commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 
-const _getDate = () => {
-  const date = new Date();
-  return date.getHours();
-};
+// const _getDate = () => {
+//   const date = new Date();
+//   return date.getHours();
+// };
 
-const Header = () => {
-  return _getDate();
-};
+/**
+ * function based components are used to just render the but getting are without getting the values from the other components
+ */
+// const Header = () => {
+//   return (
+//       <header>
+//           <div>LOGO</div>
+//           <input />
+//       </header>
+//   );
+// };
+
+/**
+ * class based components are used to get the values like props states from the rest components
+ */
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div>LOGO</div>
+        <input />
+      </header>
+    );
+  }
+}
 
 export default Header;
