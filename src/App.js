46fa6fe78e8b6159d0a6f41e7ef31d913d762934
SathishSaminathan <Sathishcse1975@gmail.com>
2018-11-16 17:21:34 +0530

    intial_commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,9 @@ class App extends Component {
     return (
       <div>
         <Header/>
-        <NewsList newsData={this.state.news}/>
+        <NewsList newsData={this.state.news}>
+          <h3>Welcome to the NewsFeed!!!</h3> {/**This will be passed as the children to the NewsList Component and we can assess them by using the "props.children" */}
+        </NewsList>
       </div>
     );
   }
