692b1e77e0b04cbeb96fafaec57387fcbb01a60d
SathishSaminathan <Sathishcse1975@gmail.com>
2018-11-16 17:16:04 +0530

    initial_commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './App.css';
+import "./App.css";
 
 import Header from "./components/header";
+import NewsList from "./components/news_list";
+import JSON from "./db.json";
 
 class App extends Component {
+
+  state={
+    news:JSON
+  }
+
   render() {
+    console.log(this.state.news)
     return (
-      <Header/>
+      <div>
+        <Header/>
+        <NewsList newsData={this.state.news}/>
+      </div>
     );
   }
 }
